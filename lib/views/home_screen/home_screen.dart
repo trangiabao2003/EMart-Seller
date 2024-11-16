f1d84e97e574af4748cb891d0bb0bfbdfb6546bc
biennc <biennc.21it@vku.udn.vn>
2024-11-16 11:54:42 +0700

    design item details

--- lib/views/home_screen/home_screen.dart
+++ lib/views/home_screen/home_screen.dart
@@ -155,42 +155,43 @@ class HomeScreen extends StatelessWidget {
                               .make(),
                           10.heightBox,
                           SingleChildScrollView(
+                              scrollDirection: Axis.horizontal,
                               child: Row(
-                            children: List.generate(
-                                6,
-                                (index) => Column(
-                                      crossAxisAlignment:
-                                          CrossAxisAlignment.start,
-                                      children: [
-                                        Image.asset(
-                                          imgP1,
-                                          width: 150,
-                                          fit: BoxFit.cover,
-                                        ),
-                                        10.heightBox,
-                                        "Laptop 8GB/256GB"
-                                            .text
-                                            .fontFamily(semibold)
-                                            .color(darkFontGrey)
-                                            .make(),
-                                        10.heightBox,
-                                        "\$600"
-                                            .text
-                                            .color(redColor)
-                                            .fontFamily(bold)
-                                            .size(16)
-                                            .make(),
-                                        10.heightBox,
-                                      ],
-                                    )
-                                        .box
-                                        .white
-                                        .margin(const EdgeInsets.symmetric(
-                                            horizontal: 4))
-                                        .roundedSM
-                                        .padding(const EdgeInsets.all(8))
-                                        .make()),
-                          ))
+                                children: List.generate(
+                                    6,
+                                    (index) => Column(
+                                          crossAxisAlignment:
+                                              CrossAxisAlignment.start,
+                                          children: [
+                                            Image.asset(
+                                              imgP1,
+                                              width: 150,
+                                              fit: BoxFit.cover,
+                                            ),
+                                            10.heightBox,
+                                            "Laptop 8GB/256GB"
+                                                .text
+                                                .fontFamily(semibold)
+                                                .color(darkFontGrey)
+                                                .make(),
+                                            10.heightBox,
+                                            "\$600"
+                                                .text
+                                                .color(redColor)
+                                                .fontFamily(bold)
+                                                .size(16)
+                                                .make(),
+                                            10.heightBox,
+                                          ],
+                                        )
+                                            .box
+                                            .white
+                                            .margin(const EdgeInsets.symmetric(
+                                                horizontal: 4))
+                                            .roundedSM
+                                            .padding(const EdgeInsets.all(8))
+                                            .make()),
+                              ))
                         ],
                       ),
                     ),
